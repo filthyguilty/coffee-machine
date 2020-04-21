c466d36db953eb46aa7aa36cb15b2d5b8170e558
filthyguilty <977445kp@gmail.com>
2020-04-21 17:57:54 +0300

    Машина готовит кофе

--- index.php
+++ index.php
@@ -4,65 +4,65 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel='stylesheet' href='style.css'>
+    <link rel="stylesheet" href="style.css">
     <title>Кофе-машина</title>
   </head>
   <body>
-    <div class='container'>
-      <div class='row coffee-machine'>
-        <div class='col-6 coffee-list d-flex-column justify-content-around'>
-          <div class= 'coffee-item'>
-            <img src='img/americano.png' alt=''>
+    <div class="container">
+      <div class="row coffee-machine" id="coffee">
+        <div class="col-6 coffee-list d-flex flex-column justify-content-around">
+          <div class="coffee-item" onclick="buyCoffee('Американо', 50, this)">
+            <img src="img/americano.png" alt="">
             <span>Американо - 50 руб.</span>
-          </div>          
-          <div class= 'coffee-item'>
-            <img src='img/cappuccino.png' alt=''>
+          </div>
+          <div class="coffee-item" onclick="buyCoffee('Капучино', 92, this)">
+            <img src="img/cappuccino.png" alt="">
             <span>Капучино - 92 руб.</span>
-          </div>          
-          <div class= 'coffee-item'>
-            <img src='img/espresso.png' alt=''>
+          </div>
+          <div class="coffee-item" onclick="buyCoffee('Эспрессо', 66, this)">
+            <img src="img/espresso.png" alt="">
             <span>Эспрессо - 66 руб.</span>
-          </div>          
-          <div class= 'coffee-item'>
-            <img src='img/latte.jpg' alt=''>
+          </div>
+          <div class="coffee-item" onclick="buyCoffee('Латте', 128, this)">
+            <img src="img/latte.jpg" alt="">
             <span>Латте - 128 руб.</span>
           </div>
         </div>
-        <div class='col-6 coffee-oper'>
+        <div class="col-6 coffee-oper">
           <div class="row p-3">
             <div class="col-6">
               <div class="display">
                 <p class="display-text">Выберите кофе</p>
                 <div class="progress">
-                  <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+                  <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 0%"></div>
                 </div>
               </div>
-              <div class='coffee-cup'>
-                <img src='img/americano.png' alt=''>
+              <div class="coffee-cup">
+                <img class="d-none" src="#" alt="">
               </div>
             </div>
-            <div class='col-6'>
+            <div class="col-6">
               <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Имя получателя" aria-label="Имя получателя" aria-describedby="basic-addon2">
+                <input type="text" class="form-control" placeholder="Баланс" aria-label="Имя получателя" aria-describedby="basic-addon2">
                 <div class="input-group-append">
-                  <span class="input-group-text" id="basic-addon2">@example.com</span>
+                  <span class="input-group-text" id="basic-addon2">&#8381;</span>
                 </div>
               </div>
               <div class="atm">
                 <img src="img/bill_acc.png" alt="">
               </div>
-              <button class='btn btn-primary btn-block mt-3'>Сдача</button>
+              <button class="btn btn-primary btn-block mt-3">Сдача</button>
               <div class="change-box mt-3"></div>
             </div>
           </div>
         </div>
       </div>
     </div>
-
     <!-- Optional JavaScript -->
+    <script src="script.js"></script>
+    <script src="coffee.js"></script>
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
---
+++ script.js
@@ -0,0 +1,98 @@
+"use strict"
+
+// window.document.documentElement.body
+// Поиск элементов
+//------------------------Устаревшие методы-----------------------------------------
+/*let coffeeMachine = document.getElementById("coffee"); //Поиск по ID
+console.log(coffeeMachine);
+let images = document.getElementsByTagName("img"); //Поиск по тэгу
+console.log(images);
+let coffeeItems = document.getElementsByClassName("coffee-item"); //Поиск по классу
+console.log(coffeeItems);
+let firstImage = coffeeItems[0].getElementsByTagName("img");
+console.log(firstImage[0]);*/
+// ----------------------------------------------------------------------------------
+//------------------------Современные методы-----------------------------------------
+/*let coffeeMachine = document.querySelector("#coffee");
+console.log(coffeeMachine);
+let image = document.querySelector("img");
+console.log(image);
+let coffeeItems = document.querySelectorAll(".coffee-item");
+console.log(coffeeItems);
+let itemImages = document.querySelectorAll(".coffee-item img")
+console.log(itemImages);
+let cupImages = document.querySelectorAll(".coffee-item img, .coffee-cup img");
+console.log(cupImages);*/
+//----------------------------------------------------------------------------------
+//------------------------------Работа с элементами---------------------------------
+//Изменение CSS свойств
+/*let coffeeMachine = document.querySelector(".coffee-machine");
+coffeeMachine.style.border = "10px solid darkblue";
+coffeeMachine.style.borderRadius = "25px";
+coffeeMachine.style.position = "absolute";
+coffeeMachine.style.top = "15px";
+coffeeMachine.style.left = "150px";
+let coffeeMachineTop = coffeeMachine.style.top;
+console.log( parseInt(coffeeMachineTop) );*/
+//Изменение аттрибутов
+/*let balance = document.querySelector("input[type='text']");
+let balanceType = balance.getAttribute("type");
+console.log(balanceType);
+balance.setAttribute("type", "date");
+console.log( balance.hasAttribute("placeholder") );
+balance.removeAttribute("aria-label");
+balance.value = 500; // == balance.setAttribute('value', 500);
+console.log(balance.value); // == balance.getAttribute('value');*/
+
+//Изменение классов
+/*let changeButton = document.querySelector(".btn");
+console.log(changeButton.classList);
+changeButton.classList.remove("btn-primary");
+changeButton.classList.add("btn-success");*/
+//changeButton.classList.toggle("ml-5") // Вкл. / Выкл.
+
+//Изменение содержимого элементов
+/*let displayText = document.querySelector(".display-text");
+console.log( displayText.innerHTML );
+console.log( displayText.innerText );
+//displayText.innerHTML = "<b>Готовим кофе</b>";
+displayText.innerText = "<b>Готовим кофе</b>";*/
+
+// Ключевое слово this
+// this возвращает объект к которому обращено свойство или обращен метод
+
+// <div class="coffee-item" onclick="buyCoffee('Американо', 50, this)">
+
+// Тоже самое что
+/*  let elem = document.querySelectorAll(".coffee-item");
+  elem[1].onclick = function () {
+    buyCoffee('Американо', 50, this);
+  }*/
+//
+
+//----------------Планирование---------------------------------------
+//таймаут
+
+/*let timeout = setTimeout(paintBody, 5000, 'aqua');
+let changeButton = document.querySelector(".btn");
+changeButton.onclick = function () {
+  clearTimeout(timeout);
+}
+setTimeout(function () {
+  paintBody('aqua');
+}, 5000);
+function paintBody(color) {
+  document.body.style.background = color;
+}*/
+
+/*let interval = setInterval(trashConsole, 1000);
+
+let changeButton = document.querySelector(".btn");
+changeButton.onclick = function () {
+  clearInterval(interval);
+}
+
+function trashConsole() {
+  console.log(Math.random());
+}
+*/
